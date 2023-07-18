4fc015c8745d2cf561623800d0bbbc21623d5028
minseong <alstjd055@gmail.com>
2023-07-18 19:06:50 +0900

    day29 setting

--- double-click-heart/script.js
+++ double-click-heart/script.js
@@ -4,7 +4,7 @@ const times = document.querySelector('#times');
 let clickTime = 0;
 let timesClicked = 0;
 
-loveMe.addEventListener('click', () => {
+loveMe.addEventListener('click', (e) => {
   if(clickTime === 0) {
     clickTime = new Date().getTime();
   } else {
