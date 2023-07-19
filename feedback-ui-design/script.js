6fee839741661ea904ebf729dd7695192bd31a0c
minseong <alstjd055@gmail.com>
2023-07-19 22:39:01 +0900

    day44

--- feedback-ui-design/script.js
+++ feedback-ui-design/script.js
@@ -1,36 +1,38 @@
 const ratings = document.querySelectorAll('.rating');
 const ratingsContainer = document.querySelector('.ratings-container');
-const sendBtn = document.getElementById('send');
-const panel = document.getElementById('panel');
-
-let selectedRating = 'satisfied';
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+let selectedRating = 'Satisfied';
 
 ratingsContainer.addEventListener('click', (e) => {
-  if (e.target.parentNode.classList.contains('rating') && e.taget.nextElementSibling) {
-    removeActive();   
-    e.target.parentNode.classList.add('active');
-    selectedRating = e.target.nextElementSibling.innerHTML;
-  } else if(e.target.parentNode.classList.contains('rating') && e.target.previousSibling && e.target.previousSibling.nodeName === 'IMG') {
-    removeActive();
-    e.target.parentNode.classList.add('active');
-    selectedRating = e.target.innerHTML;
-  }
+    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        selectedRating = e.target.nextElementSibling.innerHTML;
+    } else if(
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.previousSibling &&
+        e.target.previousElementSibling.nodeName === 'IMG'
+    ) {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        selectedRating = e.target.innerHTML;
+    }
+
 })
 
-console.log("🚀 ~ file: script.js:17 ~ ratingsContainer.addEventListener ~ selectedRating:", selectedRating)
-
-sendBtn.addEventListener('click', () => {
-  panel.innerHTML = `
-    <i class="fas fa-heart"></i>
-    <strong>감사합니다</strong>
-    <br>
-    <strong>해주신 응답: ${selectedRating}</strong>
-    <p>해주신 응답 참고하여 서비스를 개선하겠습니다.</p>
-  `;
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>감사합니다!</strong>
+        <br>
+        <strong>해주신 응답: ${selectedRating}</strong>
+        <p>해주신 응답 참고하여 서비스를 개선하겠습니다.</p>
+    `
 })
 
 function removeActive() {
   for(let i = 0; i < ratings.length; i++) {
-    ratings[i].classList.remove('active');
+      ratings[i].classList.remove('active');
   }
 }
