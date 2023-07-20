c8ec57279614f151c2a850867693916d0cdae454
minseong <alstjd055@gmail.com>
2023-07-20 19:29:37 +0900

    day46

--- testimonial-box-switcher/script.js
+++ testimonial-box-switcher/script.js
@@ -1,6 +1,6 @@
 const testimonialContainer = document.querySelector('.testimonial-container');
 const testimonial = document.querySelector('.testimonial');
-const userImage = document.querySelector('user-image');
+const userImage = document.querySelector('.user-image');
 const username = document.querySelector('.username');
 const role = document.querySelector('.role');
 
